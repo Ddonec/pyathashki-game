37b67e0196871684e3b4865a5ba65e7b40712c59
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-06 17:16:33 +0300

    feat: change loga and mobile adaptation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const gameNode = document.getElementById("game"),
-  container = document.getElementById("game"),
-  matrixItem = Array.from(container.querySelectorAll(".button")),
-  countItems = 16;
+   container = document.getElementById("game"),
+   matrixItem = Array.from(container.querySelectorAll(".button")),
+   countItems = 16;
 
 const mixBtn = document.getElementById("mix-puzzles");
 const lossBtn = document.getElementById("take-a-fall");
@@ -17,183 +17,161 @@ const goHomeBtn = document.getElementById("go-to-home-btn");
 let matrix = getMatrix(matrixItem.map((e) => Number(e.dataset.matrixId)));
 setPositionItems(matrix);
 const maxShuffles = 50,
-  shuffleClass = "gameShuffle";
+   shuffleClass = "gameShuffle";
 window.addEventListener("load", () => {
-  let e,
-    t = 0;
-  clearInterval(e),
-    gameNode.classList.add(shuffleClass),
-    (e = setInterval(() => {
-      randomSwap(matrix),
-        setPositionItems(matrix),
-        (t += 1),
-        t >= maxShuffles &&
-          ((t = 0), clearInterval(e), gameNode.classList.remove(shuffleClass));
-    }));
+   let e,
+      t = 0;
+   clearInterval(e),
+      gameNode.classList.add(shuffleClass),
+      (e = setInterval(() => {
+         randomSwap(matrix), setPositionItems(matrix), (t += 1), t >= maxShuffles && ((t = 0), clearInterval(e), gameNode.classList.remove(shuffleClass));
+      }));
 });
 let blockedCoords = null;
 function randomSwap(e) {
-  var t = findCoordinatesByNumber(blankNumber, e),
-    r = validCoords(e, blockedCoords);
-  swap(t, r[Math.floor(Math.random() * r.length)], e), (blockedCoords = t);
+   var t = findCoordinatesByNumber(blankNumber, e),
+      r = validCoords(e, blockedCoords);
+   swap(t, r[Math.floor(Math.random() * r.length)], e), (blockedCoords = t);
 }
 const blankNumber = 16;
 function getMatrix(t) {
-  const r = [[], [], [], []];
-  let n = 0,
-    o = 0;
-  for (let e = 0; e < t.length; e++)
-    4 <= o && (n++, (o = 0)), (r[n][o] = t[e]), o++;
-  return r;
+   const r = [[], [], [], []];
+   let n = 0,
+      o = 0;
+   for (let e = 0; e < t.length; e++) 4 <= o && (n++, (o = 0)), (r[n][o] = t[e]), o++;
+   return r;
 }
 function setPositionItems(r) {
-  for (let t = 0; t < r.length; t++)
-    for (let e = 0; e < r[t].length; e++) {
-      var n = r[t][e];
-      setNodeStyles(matrixItem[n - 1], e, t);
-    }
+   for (let t = 0; t < r.length; t++)
+      for (let e = 0; e < r[t].length; e++) {
+         var n = r[t][e];
+         setNodeStyles(matrixItem[n - 1], e, t);
+      }
 }
 function setNodeStyles(e, t, r) {
-  e.style.transform = `translate3D(${100 * t}%, ${100 * r}%, 0)`;
+   e.style.transform = `translate3D(${100 * t}%, ${100 * r}%, 0)`;
 }
 function shuffleArray(e) {
-  return e
-    .map((e) => ({ value: e, sort: Math.random() }))
-    .sort((e, t) => e.sort - t.sort)
-    .map(({ value: e }) => e);
+   return e
+      .map((e) => ({ value: e, sort: Math.random() }))
+      .sort((e, t) => e.sort - t.sort)
+      .map(({ value: e }) => e);
 }
 function validCoords(r, n) {
-  var o = findCoordinatesByNumber(blankNumber, r);
-  const a = [];
-  for (let t = 0; t < r.length; t++)
-    for (let e = 0; e < r[t].length; e++)
-      isValidForSwap(o, { x: e, y: t }) &&
-        ((n && n.x === e && n.y === t) || a.push({ x: e, y: t }));
-  return a;
+   var o = findCoordinatesByNumber(blankNumber, r);
+   const a = [];
+   for (let t = 0; t < r.length; t++) for (let e = 0; e < r[t].length; e++) isValidForSwap(o, { x: e, y: t }) && ((n && n.x === e && n.y === t) || a.push({ x: e, y: t }));
+   return a;
 }
 function findCoordinatesByNumber(r, n) {
-  for (let t = 0; t < n.length; t++)
-    for (let e = 0; e < n[t].length; e++)
-      if (n[t][e] === r) return { x: e, y: t };
-  return null;
+   for (let t = 0; t < n.length; t++) for (let e = 0; e < n[t].length; e++) if (n[t][e] === r) return { x: e, y: t };
+   return null;
 }
 function isValidForSwap(e, t) {
-  var r = Math.abs(e.x - t.x),
-    n = Math.abs(e.y - t.y);
-  return !((1 !== r && 1 !== n) || (e.x !== t.x && e.y !== t.y));
+   var r = Math.abs(e.x - t.x),
+      n = Math.abs(e.y - t.y);
+   return !((1 !== r && 1 !== n) || (e.x !== t.x && e.y !== t.y));
 }
 function swap(e, t, r) {
-  var n = r[e.y][e.x];
-  (r[e.y][e.x] = r[t.y][t.x]), (r[t.y][t.x] = n), isWon(r) && addWonClass();
+   var n = r[e.y][e.x];
+   (r[e.y][e.x] = r[t.y][t.x]), (r[t.y][t.x] = n), isWon(r) && addWonClass();
 }
 container.addEventListener("click", (e) => {
-  var t = e.target.closest("button");
-  !t ||
-    (isValidForSwap(
-      (e = findCoordinatesByNumber(Number(t.dataset.matrixId), matrix)),
-      (t = findCoordinatesByNumber(blankNumber, matrix))
-    ) &&
-      (swap(t, e, matrix), setPositionItems(matrix)));
+   var t = e.target.closest("button");
+   !t || (isValidForSwap((e = findCoordinatesByNumber(Number(t.dataset.matrixId), matrix)), (t = findCoordinatesByNumber(blankNumber, matrix))) && (swap(t, e, matrix), setPositionItems(matrix)));
 }),
-  window.addEventListener("keydown", (e) => {
-    if (e.key.includes("Arrow")) {
-      var t = findCoordinatesByNumber(blankNumber, matrix);
-      const n = { x: t.x, y: t.y };
-      var r = e.key.split("Arrow")[1].toLowerCase(),
-        e = matrix.length;
-      switch (r) {
-        case "up":
-          n.y += 1;
-          break;
-        case "down":
-          --n.y;
-          break;
-        case "left":
-          n.x += 1;
-          break;
-        case "right":
-          --n.x;
+   window.addEventListener("keydown", (e) => {
+      if (e.key.includes("Arrow")) {
+         var t = findCoordinatesByNumber(blankNumber, matrix);
+         const n = { x: t.x, y: t.y };
+         var r = e.key.split("Arrow")[1].toLowerCase(),
+            e = matrix.length;
+         switch (r) {
+            case "up":
+               n.y += 1;
+               break;
+            case "down":
+               --n.y;
+               break;
+            case "left":
+               n.x += 1;
+               break;
+            case "right":
+               --n.x;
+         }
+         n.y >= e || n.y < 0 || n.x >= e || n.x < 0 || (swap(t, n, matrix), setPositionItems(matrix));
       }
-      n.y >= e ||
-        n.y < 0 ||
-        n.x >= e ||
-        n.x < 0 ||
-        (swap(t, n, matrix), setPositionItems(matrix));
-    }
-  });
+   });
 const winFlatArr = new Array(16).fill(0).map((e, t) => t + 1);
 function isWon(e) {
-  var t = e.flat();
-  for (let e = 0; e < winFlatArr.length; e++)
-    if (t[e] !== winFlatArr[e]) return !1;
-  return !0;
+   var t = e.flat();
+   for (let e = 0; e < winFlatArr.length; e++) if (t[e] !== winFlatArr[e]) return !1;
+   return !0;
 }
 const wonClass = "fifteenWon";
 
 // Функция победы
 function addWonClass() {
-  setTimeout(() => {
-    container.classList.add(wonClass);
-    setTimeout(() => {
-      container.classList.remove(wonClass);
-
-      document.querySelector(".value16").classList.add("visible");
+   setTimeout(() => {
+      container.classList.add(wonClass);
       setTimeout(() => {
-        popupToWin();
-      }, 300);
-    }, 10);
-  }, 200);
+         container.classList.remove(wonClass);
+
+         document.querySelector(".value16").classList.add("visible");
+         setTimeout(() => {
+            popupToWin();
+         }, 300);
+      }, 10);
+      console.log(true);
+   }, 200);
 }
 
 mixBtn.addEventListener("click", function () {
-  let e,
-    t = 0;
-  clearInterval(e),
-    gameNode.classList.add(shuffleClass),
-    (e = setInterval(() => {
-      randomSwap(matrix),
-        setPositionItems(matrix),
-        (t += 1),
-        t >= maxShuffles &&
-          ((t = 0), clearInterval(e), gameNode.classList.remove(shuffleClass));
-    }));
+   let e,
+      t = 0;
+   clearInterval(e),
+      gameNode.classList.add(shuffleClass),
+      (e = setInterval(() => {
+         randomSwap(matrix), setPositionItems(matrix), (t += 1), t >= maxShuffles && ((t = 0), clearInterval(e), gameNode.classList.remove(shuffleClass));
+      }));
 });
 
 // Функция появления pop-up
 function popupToWin() {
-  popUpWin.classList.remove("none");
-  popUpWin.classList.add("active");
-  gBG.classList.remove("none");
-  gBG.classList.add("active");
-  container.style.display = "none";
-  newBtns.style.display = "none";
+   popUpWin.classList.remove("none");
+   popUpWin.classList.add("active");
+   gBG.classList.remove("none");
+   gBG.classList.add("active");
+   container.style.display = "none";
+   newBtns.style.display = "none";
 }
 
 function popupToWinRemowe() {
-  popUpWin.classList.add("none");
-  popUpWin.classList.remove("active");
-  gBG.classList.add("none");
-  gBG.classList.remove("active");
-  container.style.display = "block";
-  newBtns.style.display = "flex";
-  document.querySelector(".value16").classList.remove("visible")
+   popUpWin.classList.add("none");
+   popUpWin.classList.remove("active");
+   gBG.classList.add("none");
+   gBG.classList.remove("active");
+   container.style.display = "block";
+   newBtns.style.display = "flex";
+   document.querySelector(".value16").classList.remove("visible");
 }
 
 lossBtn.addEventListener("click", function () {
-  alert("Ты успешно сдался");
-  setTimeout(() => {
-    container.classList.add(wonClass);
-    setTimeout(() => {
-      container.classList.remove(wonClass);
-
-      document.querySelector(".value16").classList.add("visible");
+   alert("Ты успешно сдался");
+   setTimeout(() => {
+      container.classList.add(wonClass);
       setTimeout(() => {
-        popupToWin();
-      }, 300);
-    }, 10);
-  }, 200);
+         container.classList.remove(wonClass);
+
+         document.querySelector(".value16").classList.add("visible");
+         setTimeout(() => {
+            popupToWin();
+         }, 300);
+      }, 10);
+      console.log(true);
+   }, 200);
 });
 
 goHomeBtn.addEventListener("click", function () {
-  popupToWinRemowe();
+   popupToWinRemowe();
 });
